2ec86755dadaaeb82089aefa613f2451fe861d40
keidisSuarez <keidissuarez16@gmail.com>
2025-08-30 13:34:36 -0400

    Agregamos cambios para stilos

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <section className="text-light secctionHeader d-flex justify-content-center align-items-center">
-      <div className="text-center">
+      <div className="text-center secctionTitle">
         <h1>¡Pizzería Mamma Mía!</h1>
         <p>Tenemos las mejores Pizzas que podras encontrar</p>
       </div>
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,8 +1,5 @@
 import Header from "../components/Header";
 import CardPizza from "./CardPizza";
-import napolitana from '../assets/imgs/pizzaNapolitana.jpg';
-import espanola from '../assets/imgs/pizzaEspanola.jpg';
-import pepperoni from '../assets/imgs/pizza-pepperoni.jpg';
 
 export default function Home() {
   return (
@@ -14,21 +11,21 @@ export default function Home() {
             name="Pizza Napolitana"
             price={7990}
             ingredientes={["Mozzarella, ","Jamon, ","Oregano, ","Tomates"]}
-            img={napolitana}
+            img="/imgs/pizzaNapolitana.jpg"
             alt="Pizza Napolitana"
           />
           <CardPizza
             name="Pizza Española"
             price={9990}
             ingredientes={["Mozzarella, ","Gorgozola, ", "Provolone"]}
-            img={espanola}
+            img="/imgs/pizzaEspanola.jpg"
             alt="Pizza Espanola"
           />
           <CardPizza
             name="Pizza Pepperoni"
             price={8990}
             ingredientes={["Mozzarella, ","Pepperoni, ","Oregano"]}
-            img={pepperoni}
+            img="/imgs/pizza-pepperoni.jpg"
             alt="Pizza Pepperoni"
           />
         </div>
