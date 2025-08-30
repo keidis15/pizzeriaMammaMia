bdd7c1945c3efb9808c1d0a1dc74022bc584c905
keidisSuarez <keidissuarez16@gmail.com>
2025-08-30 12:45:19 -0400

    Arreglamos rutas de imagenes

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,5 +1,8 @@
 import Header from "../components/Header";
 import CardPizza from "./CardPizza";
+import napolitana from '../assets/imgs/pizzaNapolitana.jpg';
+import espanola from '../assets/imgs/pizzaEspanola.jpg';
+import pepperoni from '../assets/imgs/pizza-pepperoni.jpg';
 
 export default function Home() {
   return (
@@ -11,21 +14,21 @@ export default function Home() {
             name="Pizza Napolitana"
             price={7990}
             ingredientes={["Mozzarella, ","Jamon, ","Oregano, ","Tomates"]}
-            img="/pizzaNapolitana.jpg"
+            img={napolitana}
             alt="Pizza Napolitana"
           />
           <CardPizza
             name="Pizza Española"
             price={9990}
             ingredientes={["Mozzarella, ","Gorgozola, ", "Provolone"]}
-            img="/pizzaEspanola.jpg"
+            img={espanola}
             alt="Pizza Espanola"
           />
           <CardPizza
             name="Pizza Pepperoni"
             price={8990}
             ingredientes={["Mozzarella, ","Pepperoni, ","Oregano"]}
-            img="/pizza-peperoni.jpg"
+            img={pepperoni}
             alt="Pizza Pepperoni"
           />
         </div>
