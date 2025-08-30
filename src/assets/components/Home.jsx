3116f390821030b18ca1b80b7dc4c9117b4c2340
keidisSuarez <keidissuarez16@gmail.com>
2025-08-30 13:51:38 -0400

    Agregamos y mejoramos las rutas de las imagenes

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,31 +1,35 @@
 import Header from "../components/Header";
 import CardPizza from "./CardPizza";
+import napolitana from "../imgs/pizzaNapolitana.jpg";
+import espanola from "../imgs/pizzaEspanola.jpg";
+import pepperoni from "../imgs/pizza-peperoni.jpg";
 
 export default function Home() {
   return (
     <>
       <Header />
+       <img src="/imgs/pizzaNapolitana.jpg" alt="Test" />
       <div className="container">
         <div className="row ">
           <CardPizza
             name="Pizza Napolitana"
             price={7990}
             ingredientes={["Mozzarella, ","Jamon, ","Oregano, ","Tomates"]}
-            img="/imgs/pizzaNapolitana.jpg"
+            img={napolitana}
             alt="Pizza Napolitana"
           />
           <CardPizza
             name="Pizza Española"
             price={9990}
             ingredientes={["Mozzarella, ","Gorgozola, ", "Provolone"]}
-            img="/imgs/pizzaEspanola.jpg"
+            img={espanola}
             alt="Pizza Espanola"
           />
           <CardPizza
             name="Pizza Pepperoni"
             price={8990}
             ingredientes={["Mozzarella, ","Pepperoni, ","Oregano"]}
-            img="/imgs/pizza-pepperoni.jpg"
+            img={pepperoni}
             alt="Pizza Pepperoni"
           />
         </div>
